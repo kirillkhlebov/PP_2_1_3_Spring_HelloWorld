8ef6bd11d6831499ad63eb07ff912b3d2e91fe4d
Kirill Khlebov <kirill30003000@gmail.com>
2023-08-15 01:51:01 +0300

    Initial Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorldBean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("сравнение hello world: " + (helloWorldBean == helloWorldBean2));
+        System.out.println("сравнение cat" + (catBean == catBean2));
+
+        System.out.println(helloWorldBean.getMessage());
     }
 }
